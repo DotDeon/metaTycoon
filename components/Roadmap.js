6b1936bae76f0ce01a0cb43a606ff9856b486675
richfxxk <92997531+richfxxk@users.noreply.github.com>
2021-11-03 22:50:16 +0800

    Update Roadmap.js

--- components/Roadmap.js
+++ components/Roadmap.js
@@ -9,62 +9,19 @@ function Roadmap() {
       <div className="container py-24">
         <div>
           <h1 className="text-center text-white font-angkor text-4xl">
-            The Ultimate Roadmap (Whitepaper)
+            The Ultimate Roadmap Whitepaper
           </h1>
-          <p className="text-center text-white font-bold text-lg mt-4">
-            The Ultimate Roadmap is launched when the project is 100% sold
-          </p>
         </div>
         <div className=" flex justify-center md:space-x-4 flex-col md:flex-row">
           <button
             className="bg-white mt-10 px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transision duration-150"
             onClick={() =>
-              window.open("http://whitepaper.richfxxk.com/", "_blank")
+              window.open("http://whitepaper.meta-tycoon.club/", "_blank")
             }
           >
             Read Whitepaper
           </button>
-          <button
-            className="bg-white mt-10 px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transision duration-150"
-            onClick={() =>
-              window.open("https://pdfhost.io/v/KzWVDVXb~_whitepaper", "_blank")
-            }
-          >
-            Download Whitepaper
-          </button>
-          <button
-            className="bg-white mt-10 px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transision duration-150"
-            onClick={() =>
-              window.open(
-                "https://pdfhost.io/v/PS1EIAHwO_Chi_Whitepaper",
-                "_blank"
-              )
-            }
-          >
-            Download Whitepaper(Simplified Chinese)
-          </button>
-          <button
-            className="bg-white mt-10 px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transision duration-150"
-            onClick={() =>
-              window.open(
-                "https://pdfhost.io/v/N234pLUjr_Chi_Whitepaper",
-                "_blank"
-              )
-            }
-          >
-            Download Whitepaper(Traditional Chinese)
-          </button>
-        </div>
-        <div className="flex relative pt-24 min-w-screen items-center flex-col justify-center hover:scale-150 transision duration-200 ease-out">
-          <Image
-            src={a4}
-            layout="fixed"
-            objectFit="contain"
-            width={900}
-            height={880}
-            className="rounded-3xl"
-          />
-          {/* <Image src={a4} layout="fill" objectFit="contain" /> */}
+          
         </div>
       </div>
     </div>
