c4a3b1215f761d2ea7fc22e37a4644ece484640a
richfxxk <92997531+richfxxk@users.noreply.github.com>
2022-09-01 01:11:07 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
-<div className="bg-black flex flex-col">
-      <Head>
+export default function Home() {
+  return (
+    <div className="bg-black flex flex-col">
+      <Head><div className="bg-black flex flex-col">
+   
         <title>Minting Closed</title>
+</Head>
